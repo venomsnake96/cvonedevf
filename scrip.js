795a74fcd3811bf36e59897585eb9cd99e24c0a0
Gabriel Sotelo <114443003+venomsnake96@users.noreply.github.com>
2024-05-24 19:03:52 -0600

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,8 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
