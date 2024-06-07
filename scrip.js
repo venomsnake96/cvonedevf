a92e17b80a15c7696989b664dcb5eeb533c21711
VenomSnake <claudiojaimez.sec3@gmail.com>
2024-06-07 13:16:25 -0600

    for this version i delete the scrip file, looking for other options for the animations

--- scrip.js
+++
@@ -1,8 +0,0 @@
-const menuIcon = document.querySelector('#menu-icon');
-const navbar = document.querySelector('.navbar');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
-
